e976ee4b09d27543ea83dbe2e6f97ea575158c37
Michael Cardell Widerkrantz <mc@hack.org>
2015-10-27 15:01:58 +0100

    User interface in its own file.

--- irc.go
+++ irc.go
@@ -2,7 +2,6 @@
 
 package main
 
-import "bufio"
 import "fmt"
 import "time"
 import "os"
@@ -60,81 +59,6 @@ func logmsg(time time.Time, nick string, target string, text string) {
 		_, err := file.WriteString(line)
 		if err != nil {
 			fmt.Println(err)
-		}
-	}
-}
-
-func parsecommand(line string) {
-	fields := strings.Fields(line)
-
-	switch fields[0] {
-	case "/nick":
-		conn.Nick(fields[1])
-	case "/join":
-		if len(fields) != 2 {
-			commanderror("Use /join #channel\n")
-			return
-		}
-
-		currtarget = fields[1]
-		conn.Join(currtarget)
-	case "/part":
-		if len(fields) != 2 {
-			commanderror("Use /part #channel\n")
-			return
-		}
-
-		conn.Part(fields[1])
-		currtarget = ""
-	case "/names":
-		namescmd := fmt.Sprintf("NAMES %v", currtarget)
-		conn.Raw(namescmd)
-	case "/whois":
-		if len(fields) != 2 {
-			commanderror("Use /whois <nick>\n")
-			return
-		}
-
-		conn.Whois(fields[1])
-
-	case "/quit":
-		iquit()
-		if len(fields) == 2 {
-			conn.Quit(fields[1])
-		} else {
-			conn.Quit()
-		}
-		quitclient = true
-	}
-}
-
-func ui(sub bool) {
-	quitclient = false
-	for !quitclient {
-		if !sub {
-			fmt.Printf("[%v] ", currtarget)
-		}
-
-		bio := bufio.NewReader(os.Stdin)
-		line, err := bio.ReadString('\n')
-		logmsg(time.Now(), conn.Me().Nick, currtarget, line)
-
-		if err != nil {
-			log.Fatal("Couldn't get input.\n")
-		}
-
-		if line != "\n" && line != "\r\n" {
-			if line[0] == '/' {
-				// A command
-				parsecommand(line)
-			} else {
-				// Send line to target.
-				if currtarget == "" {
-					notarget()
-				} else {
-					conn.Privmsg(currtarget, line)
-				}
-			}
 		}
 	}
 }
